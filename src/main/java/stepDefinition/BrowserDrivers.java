0cfb44725c757bc86bb6c173ac7ef690e957085c
Megha Ruparelia <rupareliamegha@gmail.com>
2018-07-09 22:45:24 -0500

    Update BrowserDrivers.java
    added comments

--- src/main/java/stepDefinition/BrowserDrivers.java
+++ src/main/java/stepDefinition/BrowserDrivers.java
@@ -10,10 +10,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
-//-Dbrowser.name=firefox 
+//this class will select appropriate webdrivers for cross browser testing
 public class BrowserDrivers {
 
 	    public static WebDriver createWebDriver() {
+		    //selecting browser on which the test should run
 	        String webdriver = System.getProperty("browser", "chrome");
 	        switch(webdriver) {
 	            case "firefox":
